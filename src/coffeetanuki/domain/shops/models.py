22c86a7a455225d67c746d162412588db02d4a2f
Patrick Armengol <patrickarmengol@protonmail.com>
2023-05-18 23:06:14 +0000

    updated sqla base model names

--- src/coffeetanuki/domain/shops/models.py
+++ src/coffeetanuki/domain/shops/models.py
@@ -1,5 +1,5 @@
 from geoalchemy2 import Geography
-from litestar.contrib.sqlalchemy.base import Base
+from litestar.contrib.sqlalchemy.base import UUIDBase
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 __all__ = ["Shop"]
 
 
-class Shop(Base):
+class Shop(UUIDBase):
     name: Mapped[str] = mapped_column(String(), unique=True)
     address: Mapped[str]
     coordinates: Mapped[Geography] = mapped_column(
