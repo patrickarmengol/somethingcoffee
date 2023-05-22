8a060ae24d24083f46cad0659c4663a98c65ff8d
Patrick Armengol <patrickarmengol@protonmail.com>
2023-05-22 19:07:54 +0000

    added amenity model and relationships

--- src/coffeetanuki/domain/shops/models.py
+++ src/coffeetanuki/domain/shops/models.py
@@ -1,10 +1,28 @@
+from __future__ import annotations
 from geoalchemy2 import Geography
 from litestar.contrib.sqlalchemy.base import UUIDBase
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, ForeignKey, String, Table
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
-__all__ = ["Shop"]
+__all__ = ["Shop", "Amenity", "shop_amenity"]
+
+
+shop_amenity = Table(
+    "shop_amenity",
+    UUIDBase.metadata,
+    Column("shop_id", ForeignKey("shop.id")),
+    Column("amenity_id", ForeignKey("amenity.id")),
+)
+
+
+class Amenity(UUIDBase):
+    name: Mapped[str]
+
+    shops: Mapped[list[Shop]] = relationship(
+        secondary=shop_amenity,
+        back_populates="amenities",
+    )
 
 
 class Shop(UUIDBase):
@@ -20,3 +38,8 @@ class Shop(UUIDBase):
     )
     roaster: Mapped[str | None]
     hours_of_operation: Mapped[str | None]
+
+    amenities: Mapped[list[Amenity]] = relationship(
+        secondary=shop_amenity,
+        back_populates="shops",
+    )
