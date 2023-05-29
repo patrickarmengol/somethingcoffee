9445f0e5e64a9abb23ec35454d801696c8ad8172
Patrick Armengol <42473149+patrickarmengol@users.noreply.github.com>
2023-05-29 18:03:30 +0000

    added amenities (#17)
    * added amenity model and relationships
    
    * added amenity migration
    
    * disabled expire_on_commit for async sqla compat
    
    * added unique constraint to amenity names
    
    * added simple operations for amenities

--- src/coffeetanuki/database.py
+++ src/coffeetanuki/database.py
@@ -1,12 +1,22 @@
+from litestar.contrib.sqlalchemy.plugins import AsyncSessionConfig
 from litestar.contrib.sqlalchemy.plugins.init import (
     SQLAlchemyAsyncConfig,
     SQLAlchemyInitPlugin,
 )
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from coffeetanuki.settings import db
 
+engine = create_async_engine(db.URL)
+
+async_session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+)
+
 # TODO: replace with env vars parsed in settings.py
 sqlalchemy_config = SQLAlchemyAsyncConfig(
-    connection_string=db.URL,
+    engine_instance=engine,
+    session_maker=async_session_factory,
 )
 sqlalchemy_plugin = SQLAlchemyInitPlugin(config=sqlalchemy_config)
