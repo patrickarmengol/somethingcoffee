82b2a5cd421751e4deea4da3483fe829a5731c03
Patrick Armengol <patrickarmengol@protonmail.com>
2023-06-02 17:47:25 +0000

    removed shop frontend temporarily

--- src/coffeetanuki/domain/web/controllers.py
+++ src/coffeetanuki/domain/web/controllers.py
@@ -1,4 +1,3 @@
-from uuid import UUID
 from litestar import Controller, get
 from litestar.response_containers import Template
 
@@ -11,11 +10,3 @@ class WebController(Controller):
     @get(path="/map", include_in_schema=False)
     async def homepage(self) -> Template:
         return Template(name="map.html.jinja2")
-
-    @get(path="/shops", include_in_schema=False)
-    async def shops_list_page(self) -> Template:
-        return Template(name="shops_list.html.jinja2")
-
-    @get(path="/shops/{shop_id:uuid}", include_in_schema=False)
-    async def shop_details_page(self, shop_id: UUID) -> Template:
-        return Template(name="shop.html.jinja2")
