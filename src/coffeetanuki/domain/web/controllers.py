cff0a7882d78d0774413a592fa720e5ec36fd935
Patrick Armengol <patrickarmengol@protonmail.com>
2023-06-09 20:53:21 +0000

    moved leaflet pop to backend to avoid waiting on api call

--- src/coffeetanuki/domain/web/controllers.py
+++ src/coffeetanuki/domain/web/controllers.py
@@ -1,5 +1,14 @@
 from litestar import Controller, get
+from litestar.di import Provide
 from litestar.response_containers import Template
+from pydantic import parse_obj_as
+
+from coffeetanuki.domain.shops.utils import geojsonify
+from coffeetanuki.domain.shops.controllers import (
+    ShopRepository,
+    provide_shop_repo,
+    schemas,
+)
 
 
 class WebController(Controller):
@@ -11,6 +20,14 @@ class WebController(Controller):
     async def home_page(self) -> Template:
         return Template(name="views/home.html.jinja")
 
-    @get(path="/map", include_in_schema=False)
-    async def map_page(self) -> Template:
-        return Template(name="views/map.html.jinja")
+    @get(
+        path="/map",
+        include_in_schema=False,
+        dependencies={"shop_repo": Provide(provide_shop_repo)},
+    )
+    async def map_page(self, shop_repo: ShopRepository) -> Template:
+        shops = parse_obj_as(list[schemas.ShopDB], await shop_repo.list())
+        shops_geojson = geojsonify(shops)
+        return Template(
+            name="views/map.html.jinja", context={"shops_geojson": shops_geojson}
+        )
--- src/coffeetanuki/domain/web/static/scripts/map.js
+++ src/coffeetanuki/domain/web/static/scripts/map.js
@@ -3,20 +3,20 @@ const copy =
 const url = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
 const osm = L.tileLayer(url, { attribution: copy });
 const map = L.map("map", { layers: [osm], minZoom: 5 });
-map.setView([35.68, 139.76], 13);
-
-async function load_markers() {
-  const markers_url = `/api/shops/geojson`;
-  const response = await fetch(markers_url);
-  const geojson = await response.json();
-  return geojson;
-}
-
-async function render_markers() {
-  const markers = await load_markers();
-  L.geoJSON(markers)
-    .bindPopup((layer) => layer.feature.properties.name)
-    .addTo(map);
-}
-
-render_markers();
+// map.setView([0, 0], 13);
+//
+// async function load_markers() {
+//   const markers_url = `/api/shops/geojson`;
+//   const response = await fetch(markers_url);
+//   const geojson = await response.json();
+//   return geojson;
+// }
+//
+// async function render_markers() {
+//   const markers = await load_markers();
+//   L.geoJSON(markers)
+//     .bindPopup((layer) => layer.feature.properties.name)
+//     .addTo(map);
+// }
+//
+// render_markers();
